89d4110ff6c6448df58648e1cbb129882b284867
Puru Mohite <purumohite44@gmail.com>
2024-10-17 14:33:35 +0530

    3- useFollow hook added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -42,7 +42,7 @@ export const followUnfollowUser = async (req, res) => {
       //Unfollow the user
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
-      //TODO return the id of the user as a response
+
       res.status(200).json({ message: "User unfollowed successfully" });
     } else {
       //Follow the user
@@ -57,7 +57,6 @@ export const followUnfollowUser = async (req, res) => {
 
       await newNotification.save();
 
-      //TODO return the id of the user as a response
       res.status(200).json({ message: "User followed successfully" });
     }
   } catch (error) {
@@ -111,11 +110,9 @@ export const updateUser = async (req, res) => {
       (!newPassword && currentPassword) ||
       (!currentPassword && newPassword)
     ) {
-      return res
-        .status(400)
-        .json({
-          error: "Please provide both current password and new password",
-        });
+      return res.status(400).json({
+        error: "Please provide both current password and new password",
+      });
     }
 
     if (currentPassword && newPassword) {
